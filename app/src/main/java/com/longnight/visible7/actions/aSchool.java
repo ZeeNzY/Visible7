31157ab77838f2e0a7bc86e883b922f4fbebc305
Luuyhisa <216060117@mycput.ac.za>
2020-09-12 16:18:16 +0200

    init

---
+++ app/src/main/java/com/longnight/visible7/actions/aSchool.java
@@ -0,0 +1,19 @@
+package com.longnight.visible7.actions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.longnight.visible7.R;
+
+public class aSchool extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a_school);
+
+
+
+    }
+}
