faa3534243063f5eb355f570ca6c78112ddbc34d
Luuyhisa <216060117@mycput.ac.za>
2020-09-12 15:23:00 +0200

    init

---
+++ app/src/main/java/com/longnight/visible7/logins/Parent.java
@@ -0,0 +1,18 @@
+package com.longnight.visible7.logins;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Parent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+
+
+    }
+}
